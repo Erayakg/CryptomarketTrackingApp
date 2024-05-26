0ad42942539c2a49699baf0b347266964508f79b
Eray Akgül <90953626+Erayakg@users.noreply.github.com>
2024-05-26 10:52:58 +0300

    update entity

---
+++ src/main/java/com/crypto/base/entities/Comment.java
@@ -0,0 +1,93 @@
+package com.crypto.base.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String text;
+
+    public Comment() {
+    }
+
+    public Comment(Long id, Portfolio portfolio, User user, String text) {
+        this.id = id;
+        this.portfolio = portfolio;
+        this.user = user;
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id) && Objects.equals(portfolio, comment.portfolio) && Objects.equals(user, comment.user) && Objects.equals(text, comment.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, portfolio, user, text);
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", portfolio=" + portfolio +
+                ", user=" + user +
+                ", text='" + text + '\'' +
+                ", createDate=" + createDate +
+                ", updateDate=" + updateDate +
+                ", createdBy=" + createdBy +
+                ", updatedBy=" + updatedBy +
+                '}';
+    }
+}
